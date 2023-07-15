f8cbd6b5b87f323a1133569ed8b4094c8e76f7e5
JoseEscalona18 <114792259+JoseEscalona18@users.noreply.github.com>
2023-07-15 16:26:36 -0400

    Cositas en el js y css

--- app.js
+++ app.js
@@ -7,9 +7,11 @@ class Contacto {
 }
 
 function agregarContacto(contacto) {
+
     const contactos = JSON.parse(localStorage.getItem("contactos"));
     contactos.push(contacto);
     localStorage.setItem("contactos", JSON.stringify(contactos));
+
 }
 
 function mostrarContactos() {
@@ -44,12 +46,15 @@ function mostrarContactos() {
 }
 
 function limpiarFormulario() {
+
     document.getElementById("nombre").value = "";
     document.getElementById("apellido").value = "";
     document.getElementById("telefono").value = "";
+
   }
 
 document.getElementById("agregar").addEventListener("click", () => {
+
     const nombre = document.getElementById("nombre").value;
     const apellido = document.getElementById("apellido").value;
     const telefono = document.getElementById("telefono").value;
@@ -59,6 +64,40 @@ document.getElementById("agregar").addEventListener("click", () => {
     limpiarFormulario()
     mostrarContactos();
     }
+
+    if(!nombre){
+        document.getElementById("nombre").classList.remove("#input")
+        document.getElementById("nombre").classList.add("faltanDatos")
+        document.querySelector('#nombre').placeholder = ('Ingresa el nombre')
+        setTimeout(() => {
+            document.getElementById("nombre").classList.remove("faltanDatos")
+            document.getElementById("nombre").classList.add("#input")
+            document.querySelector('#nombre').placeholder = ('Nombre')
+          }, 2000);
+    }
+
+    if(!apellido){
+        document.getElementById("apellido").classList.remove("#input")
+        document.getElementById("apellido").classList.add("faltanDatos")
+        document.querySelector('#apellido').placeholder = ('Ingresa el apellido')
+        setTimeout(() => {
+            document.getElementById("apellido").classList.remove("faltanDatos")
+            document.getElementById("apellido").classList.add("#input")
+            document.querySelector('#apellido').placeholder = ('Apellido')
+          }, 2000);
+    }
+
+    if(!telefono){
+        document.getElementById("telefono").classList.remove("#input")
+        document.getElementById("telefono").classList.add("faltanDatos")
+        document.querySelector('#telefono').placeholder = ('Ingresa el número de teléfono')
+        setTimeout(() => {
+            document.getElementById("telefono").classList.remove("faltanDatos")
+            document.getElementById("telefono").classList.add("#input")
+            document.querySelector('#telefono').placeholder = ('Teléfono')
+          }, 2000);
+    }
+
   });
 
 mostrarContactos()
